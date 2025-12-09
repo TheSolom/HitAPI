c4c9d975c6b891ce014b84828b20a40b5a43027b
TheSolom <97428300+TheSolom@users.noreply.github.com>
2025-12-09 16:21:22 +0200

    refactor(entity): add non-null constraint to relationships and add index

--- Backend/src/modules/teams/entities/team-member.entity.ts
+++ Backend/src/modules/teams/entities/team-member.entity.ts
@@ -5,6 +5,7 @@ import {
     ManyToOne,
     CreateDateColumn,
     DeleteDateColumn,
+    Index,
     type Relation,
 } from 'typeorm';
 import { TeamMemberRoles } from '../enums/team-member-roles.enum.js';
@@ -12,6 +13,7 @@ import { Team } from './team.entity.js';
 import { User } from '../../users/entities/user.entity.js';
 
 @Entity()
+@Index(['team', 'user'], { unique: true })
 export class TeamMember {
     @PrimaryGeneratedColumn('uuid')
     id: string;
@@ -25,11 +27,13 @@ export class TeamMember {
 
     @ManyToOne(() => Team, (team) => team.teamMembers, {
         onDelete: 'CASCADE',
+        nullable: false,
     })
     team: Relation<Team>;
 
     @ManyToOne(() => User, (user) => user.teamMembers, {
         onDelete: 'CASCADE',
+        nullable: false,
     })
     user: Relation<User>;
 
