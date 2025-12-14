c3901b406f454841c8b3c194c6879bf8d67a6684
TheSolom <97428300+TheSolom@users.noreply.github.com>
2025-12-14 19:51:53 +0200

    refactor: add expose keyword

--- Backend/src/modules/auth/sessions/dto/user-session.dto.ts
+++ Backend/src/modules/auth/sessions/dto/user-session.dto.ts
@@ -1,18 +1,24 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class UserSessionDto {
+    @Expose()
     @ApiProperty({ type: 'string' })
     id: string;
 
+    @Expose()
     @ApiPropertyOptional({ type: 'string' })
     deviceInfo?: string;
 
+    @Expose()
     @ApiPropertyOptional({ type: 'string' })
     ipAddress?: string;
 
+    @Expose()
     @ApiPropertyOptional({ type: Date })
     lastUsedAt?: Date;
 
+    @Expose()
     @ApiProperty({ type: Date })
     createdAt: Date;
 }
