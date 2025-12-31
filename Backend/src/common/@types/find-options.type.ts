39961659097f923c662791cb24b5ec38e52e8c73
TheSolom <97428300+TheSolom@users.noreply.github.com>
2025-12-31 23:44:29 +0200

    refactor: make where optional and replace order with enum type

--- Backend/src/common/@types/find-options.type.ts
+++ Backend/src/common/@types/find-options.type.ts
@@ -1,10 +1,11 @@
 import { FindOptionsWhere } from 'typeorm';
+import type { OrderDirection } from '../enums/order-direction.enum.js';
 
-export type FindOptions<T> = {
-    where: FindOptionsWhere<T>[] | FindOptionsWhere<T>;
+export type FindOptions<T = unknown> = {
+    where?: FindOptionsWhere<T>[] | FindOptionsWhere<T>;
     select?: string[];
     relations?: string[];
-    order?: Record<string, 'ASC' | 'DESC'>;
+    order?: OrderDirection;
     skip?: number;
     take?: number;
 };
