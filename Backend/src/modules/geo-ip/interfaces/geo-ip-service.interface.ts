ceb5e38caad7a2ad8edf91363515413e2f3b39ef
TheSolom <97428300+TheSolom@users.noreply.github.com>
2026-01-09 19:32:16 +0200

    docs(geoIP): add jsdoc documentation to IGeoIPService interface

--- Backend/src/modules/geo-ip/interfaces/geo-ip-service.interface.ts
+++ Backend/src/modules/geo-ip/interfaces/geo-ip-service.interface.ts
@@ -2,5 +2,10 @@ import type { NullableType } from '../../../common/@types/nullable.type.js';
 import type { CountryResponseDto } from '../dto/country-response.dto.js';
 
 export interface IGeoIPService {
+    /**
+     * Get country's name and code by IP
+     * @param ip IP address
+     * @returns {NullableType<CountryResponseDto>} Country's details or null
+     */
     getCountry(ip: string): NullableType<CountryResponseDto>;
 }
