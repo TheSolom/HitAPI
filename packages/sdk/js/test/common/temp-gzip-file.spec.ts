05cb14ad5a6d5cb2393229c04c4b682f25b70425
TheSolom <97428300+TheSolom@users.noreply.github.com>
2026-03-08 01:09:08 +0200

    test: add unit tests for temp gzip file

---
+++ packages/sdk/js/test/common/temp-gzip-file.spec.ts
@@ -0,0 +1,25 @@
+import { setImmediate } from 'node:timers/promises';
+import { gunzipSync } from 'node:zlib';
+import TempGzipFile from '../../src/common/core/temp-gzip-file.js';
+
+describe('Temporary gzip file', () => {
+    it('End to end', async () => {
+        const file = new TempGzipFile('test');
+        expect(file.size).toBe(0);
+
+        await file.writeLine(Buffer.from('test1'));
+        await file.writeLine(Buffer.from('test2'));
+
+        // Wait for the next event loop cycle to ensure gzip stream has flushed to file
+        await setImmediate();
+        expect(file.size).toBeGreaterThan(0);
+
+        await file.close();
+
+        const compressedData = await file.getContent();
+        const content = gunzipSync(compressedData).toString();
+        expect(content).toBe('test1\ntest2\n');
+
+        await file.delete();
+    });
+});
