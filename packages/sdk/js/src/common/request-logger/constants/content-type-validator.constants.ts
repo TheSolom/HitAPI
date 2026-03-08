e57b2eb1f833e886de9d40ebe602956d4907f9b6
TheSolom <97428300+TheSolom@users.noreply.github.com>
2026-03-08 20:43:53 +0200

    feat(request-logger): add constants

---
+++ packages/sdk/js/src/common/request-logger/constants/content-type-validator.constants.ts
@@ -0,0 +1,9 @@
+export const ALLOWED_CONTENT_TYPES = [
+    'application/json',
+    'application/ld+json',
+    'application/problem+json',
+    'application/vnd.api+json',
+    'application/x-ndjson',
+    'text/plain',
+    'text/html',
+] as const;
---
+++ packages/sdk/js/src/common/request-logger/constants/request-logger.constants.ts
@@ -0,0 +1,3 @@
+export const TRUNCATED_SUFFIX = '... (truncated)';
+export const MAX_BODY_SIZE = 50000; // 50 KB (uncompressed)
+export const MAINTAIN_INTERVAL_MS = 1000;
