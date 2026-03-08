8b460319ff3f667bd49108f9a3e24505d394abfb
TheSolom <97428300+TheSolom@users.noreply.github.com>
2026-03-08 20:52:56 +0200

    feat(request-logger): export constants in index file

--- packages/sdk/js/src/common/request-logger/constants/index.ts
+++ packages/sdk/js/src/common/request-logger/constants/index.ts
@@ -1,3 +1,5 @@
+export * from './content-type-validator.constants.js';
 export * from './data-masker.constants.js';
 export * from './file-manager.constants.js';
 export * from './request-filter.constants.js';
+export * from './request-logger.constants.js';
