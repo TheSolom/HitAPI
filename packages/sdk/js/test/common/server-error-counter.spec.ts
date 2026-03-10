31af4728ca684f184e65f7d87e4959d7295c65a0
TheSolom <97428300+TheSolom@users.noreply.github.com>
2026-03-10 20:10:59 +0200

    test: add unit tests for service error counter file

---
+++ packages/sdk/js/test/common/server-error-counter.spec.ts
@@ -0,0 +1,23 @@
+import { RestfulMethod } from '@hitapi/shared/enums';
+import ServerErrorCounter from '../../src/common/core/server-error-counter.js';
+
+describe('Server error counter', () => {
+    it('Message and stacktrace truncation', () => {
+        const serverErrorCounter = new ServerErrorCounter();
+        const serverError = {
+            consumer: 'test',
+            method: RestfulMethod.GET,
+            path: '/test',
+            type: 'error',
+            msg: 'a'.repeat(3000),
+            traceback: 'one line\n'.repeat(8000),
+        };
+        serverErrorCounter.addServerError(serverError);
+        const serverErrors = serverErrorCounter.getAndResetServerErrors();
+        expect(serverErrors.length).toBe(1);
+        expect(serverErrors[0].msg.length).toBe(2048);
+        expect(serverErrors[0].msg).toContain('(truncated)');
+        expect(serverErrors[0].traceback.length).toBeLessThan(65536);
+        expect(serverErrors[0].traceback).toContain('(truncated)');
+    });
+});
