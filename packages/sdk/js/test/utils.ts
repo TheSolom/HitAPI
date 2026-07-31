74ec3fa5f2439de5cd59a96c5fe75052dec5e1ea
TheSolom <97428300+TheSolom@users.noreply.github.com>
2026-07-31 20:38:03 +0300

    refactor(utils): replace hardcoded HITAPI_BASE_URL with dynamic getApiUrl function

--- packages/sdk/js/test/utils.ts
+++ packages/sdk/js/test/utils.ts
@@ -1,7 +1,13 @@
 import nock from 'nock';
 
+const getApiUrl = (): string => {
+    const baseURL = process.env.API_URL || 'http://localhost:3000/api';
+    const version = process.env.API_VERSION || '1';
+    return `${baseURL}/v${version}/ingest/`;
+};
+
 export const CLIENT_ID = '65973e74-3b71-4392-bbfa-d108a8a5d9d8';
-export const HITAPI_BASE_URL = 'http://localhost:3000/api/v1/ingest';
+export const HITAPI_BASE_URL = getApiUrl();
 
 export const mockHitAPIHub = () => {
     nock(HITAPI_BASE_URL, { reqheaders: { 'X-Client-ID': CLIENT_ID } })
